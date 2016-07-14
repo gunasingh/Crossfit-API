53b1b595eb6868e3b081903fb97439f27ff74ee1
Guna Singh Chidambaram <gunasingh.1@gmail.com>
2016-07-14 08:08:02 -0700

    Added Token model and DELETE /users/login route

--- db.js
+++ db.js
@@ -18,6 +18,7 @@ var db = {};
 
 db.move = sequelize.import(__dirname + '/models/move.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
+db.token = sequelize.import(__dirname + '/models/token.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
